c9570b6bfd7def9d6c3bec89046d5ade588482cf
Sarah1212Ade <adeniyisarah1212@gmail.com>
2022-09-08 22:20:55 +0000

    corrected

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
 #include <unistd.h>
 
 /**
- * main - entry point
+ * main -Entry point
  *
- * Description: using unistd header file
- * program prints and that piece of art is useful" - Dora Korpar, 2015-10-19
- * Return: 1
+ * Description: using write function
+ * this program print and that piece ofart is useful" - Dora Korpar, 2015-10-19
+ * Return: 1 on Success
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19",  59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
