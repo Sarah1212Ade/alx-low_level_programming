c25e805a75bb49e8bc6c1f9bdb448b22de0819ba
Sarah1212Ade <adeniyisarah1212@gmail.com>
2022-09-08 21:38:02 +0000

    put.c

---
+++ 0x00-hello_world/4-put.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - entry point
+ *
+ * Description: using puts function
+ * this program prints "Programming is like building a multilingual puzzle
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
