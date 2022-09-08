a25ef9e8961faea512cb3df6d0f2f23089512e0e
Sarah1212Ade <adeniyisarah1212@gmail.com>
2022-09-08 21:59:54 +0000

    Size

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: using printf function
+ * program prints with proper grammar, but the outcome is a piece of art,
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
